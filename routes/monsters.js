aed92479b7d53568831f56a8ef878f00a0c3dc3c
RubenSmartPractice <ruben@smartpractice.co.za>
2022-02-28 14:40:50 +0200

    Change

--- routes/monsters.js
+++ routes/monsters.js
@@ -39,45 +39,6 @@ router.post('/submit', async (req, res) => {
 });
 
 
-const express = require('express');
-const People = require('../models/monsters');
-const router = express.Router();
-
-
-router.get('/',async (res,req)=>{
-   const character=  await People.find({})
-  console.log(character)
-  req.render('home/monsters' ,{character})
-  })
-
-router.post('/submit', async (req, res) => {
-   try {
-    const {fullname, RaceAlign,ArmorClass,HitPoints,Dexterity,Strength,Constitution
-   ,Intelligence,Wisdom,Charisma,Speed
-   } = req.body;
-    const people = new People({
-        fullname,
-        RaceAlign,
-        ArmorClass,
-        HitPoints,
-        Dexterity,
-        Strength,
-        Constitution,
-        Intelligence,
-        Wisdom,
-        Charisma,
-        Speed,
-        
-     })
-     await people.save();
-   
-      res.redirect('/monsters');
-     
-   } catch(e) {
-      console.log(e);
-      res.sendStatus(500);
-   }
-});
 
 
 module.exports = router;
